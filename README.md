fcfaa675699d5ff18fcb58278a0cd84666983482
Shirley <shirleey.yu@gmail.com>
2019-10-04 18:36:28 -0700

    hopefully working

---
+++ README.md
@@ -0,0 +1,19 @@
+# URL Shortener API
+
+### User Stories
+
+1. I can POST a URL to `[project_url]/api/shorturl/new` and I will receive a shortened URL in the JSON response. Example : `{"original_url":"https://www.google.com","short_url":1}`
+2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.<domain_name>.<extension>(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`.
+3. When I visit the shortened URL, it will redirect me to my original link.
+
+#### Creation Example:
+
+POST [project_url]/api/shorturl/new - body (urlencoded) : url=https://www.google.com
+
+#### Usage:
+
+[this_project_url]/api/shorturl/1
+
+#### Will redirect to:
+
+https://www.google.com
---
+++ server.js
@@ -0,0 +1,113 @@
+"use strict";
+
+var express = require("express");
+var mongo = require("mongodb");
+var mongoose = require("mongoose");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// require("dotenv").config();
+var port = process.env.PORT || 3000;
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true })
+  .catch(error => {
+    console.log("connection error");
+    console.log(error);
+  });
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+// app.get("/test", (req, res) => {
+//   res.json({ hi: "hi" });
+// });
+
+const Schema = mongoose.Schema;
+var urlSchema = new Schema({
+  original: String,
+  short: Number
+});
+
+mongoose.set("debug", true);
+
+var Url = mongoose.model("Url", urlSchema);
+
+app.post("/new", function(req, res) {
+  var orig_url = req.body["url"];
+
+  // validate url
+  var valid = validateUrl(orig_url);
+  if (!valid) {
+    res.json({ error: "invalid Url" });
+  } else {
+    // check if url already exists in db
+    Url.findOne({ original: orig_url }, (err, data) => {
+      if (err) return res.send("Error occurred when querying database");
+      if (data) {
+        res.json({ original_url: orig_url, short_url: data["short"] });
+      } else {
+        // create new entry in db
+
+        // count number of entries
+        var count = 0;
+        Url.find({}, (err, data) => {
+          if (err) return res.send("Error occurred when querying database");
+          count = data.length + 1;
+          var addUrl = new Url({
+            original: orig_url,
+            short: count
+          });
+          addUrl.save(err => {
+            if (err) {
+              return res.send("Error occurred saving entry to database");
+            }
+            res.json({ original_url: orig_url, short_url: count });
+          });
+        });
+      }
+    });
+  }
+});
+
+app.get("/:shorturl", function(req, res) {
+  var short = req.params.shorturl;
+  // check if shorturl is a valid integer
+  if (isNaN(short)) {
+    return res.json({ error: "Wrong Format" });
+  }
+  // convert to number
+  +short;
+  // check if shortened url has been previously registered in db
+  Url.findOne({ short: short }, (err, data) => {
+    if (err) {
+      return res.send("Error occurred when querying database");
+    }
+    if (data) {
+      res.redirect(data["original"]);
+    } else {
+      res.json({ error: "No short url found for given input" });
+    }
+  });
+});
+
+app.listen(port, function() {
+  console.log("Node.js listening on port", port);
+});
+
+function validateUrl(url) {
+  var regex = new RegExp(
+    // format: http(s)://www.<domain_name>.<extension>(/more/routes)
+    // entries in parentheses () are optional
+    "^(http://www.|https://www.)[a-z0-9]+([-.]{1}[a-z0-9]+)*.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?$"
+  );
+  return regex.test(url);
+}
